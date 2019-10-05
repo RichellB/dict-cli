a08e3a470afbae5177bfcf13c23dd55dc3e43d4a
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-05 08:07:52 +0000

    Modified cli.rb

--- lib/dict/cli.rb
+++ lib/dict/cli.rb
@@ -1,8 +1,14 @@
-require "dict/cli/version"
+#require "dict/cli/version"
+require "open-uri"
+require "nokogiri"
+require "pry"
 
-module Dict
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
+def run 
+    puts "Welcome to Today's Best Sellers!"
+    puts "Main Menu"
+    puts "Please enter a book to get the full review:"
+  
+    html = open("https://www.kirkusreviews.com")
+    doc = Nokogiri::HTML(html)
+    binding.pry 
 end
