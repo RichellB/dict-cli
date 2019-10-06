afa36cd100512efb35584f4e04da022ebf6be76a
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-06 02:41:53 +0000

    Modified bk_cli.rb file

--- lib/bk_cli.rb
+++ lib/bk_cli.rb
@@ -3,8 +3,9 @@ require "pry"
 class CLI
   
   def run 
-    puts "Welcome to Today's Best Sellers!"
-    puts "Main Menu"
+    puts " "
+    puts "Welcome to the reviews for Today's Best Sellers!"
+    puts "Here is a list of the books on today's Best Sellers:"
     Scraper.scrape_reviews
    
     Book.all.each.with_index(1) do |book, i|
@@ -15,17 +16,17 @@ class CLI
   
   def menu
     books = []
-    puts "Please enter a book to get the full review:"
+    puts "Please enter a book to view Krikus' full review on it:"
     input = gets.chomp
     book = Book.all[input.to_i-1]
     
     if !book 
-      puts "This book is not on the list. Please enter a valid option."
+      puts "Sorry, this book is not on the list for today! Please enter a valid option from the list above."
       menu 
     else 
-      puts "Hi"
       Scraper.scrape_book_review(book)
       puts "Here are the review details for #{book.title}:"
+      puts "*********************************************"
       puts "Book Name: #{book.book_name}"
       puts "Author: #{book.author}"
       puts "Kirkus Review: #{book.review}"
