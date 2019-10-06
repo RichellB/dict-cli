c82f982366e90ffd43b2c5428765ee2280abf264
Richell Bonfield <richell.bonfield12@gmail.com>
2019-10-06 03:06:29 +0000

    Modified scraper.rb file

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,9 +34,9 @@ KIRKUS_URL = "https://www.kirkusreviews.com"
     html = open(KIRKUS_URL+book.url)
     doc = Nokogiri::HTML(html)
     binding.pry
-    book.book_name = doc.css(".book_detail_title").text
-    book.author = doc.css(".book-author.clearfix").text.split(/[\n,]/)[5].strip
-    book.review = doc.css(".book-review-txt").text.split(/[\n,]/).join(" ")
+    book.book_name = doc.css(".book_detail_title span").text
+    book.author = doc.css(".book-author.clearfix span")[0].text
+    book.review = doc.css(".book-review-txt p").text.split(/[\n,]/).join(" ")
     
   end
 end
